fb987599e8e827e9879b29b5564a4c57be02edd4
Matt Schwager <schwag09@gmail.com>
2023-02-11 15:36:53 -0700

    Add Sanic authenticated route rules

--- tests/test_rules/sanic.py
+++ tests/test_rules/sanic.py
@@ -1,4 +1,6 @@
 from sanic import Sanic
+from sanic_jwt.decorators import protected
+from sanic_security.authentication import requires_authentication
 
 app = Sanic("MyHelloWorldApp")
 
@@ -17,3 +19,20 @@ app.add_route(handler, "/test")
 @app.get("/test")
 async def handler(request):
     return text("OK")
+
+
+# ruleid: sanic-route-authenticated
+@app.route("/protected")
+@protected()
+async def protected_route(request):
+    return json({"protected": True})
+
+
+# ruleid: sanic-route-authenticated
+@app.post("api/security/auth")
+@requires_authentication()
+async def on_authenticate(request, authentication_session):
+    return json(
+        "You have been authenticated.",
+        authentication_session.bearer.json(),
+    )
