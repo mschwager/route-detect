d7a03b0325d9b24f973e57a5c482157a94cd3d10
Matt Schwager <schwag09@gmail.com>
2023-02-26 16:45:58 -0700

    Add support for Ruby Grape authenticated routes

--- tests/test_rules/grape.rb
+++ tests/test_rules/grape.rb
@@ -14,16 +14,16 @@ module Twitter
       end
     end
 
-    # ruleid: grape-route
+    # todoruleid: grape-route-authenticated
     resource :statuses do
       desc 'Return a public timeline.'
-      # ruleid: grape-route
+      # todoruleid: grape-route-authenticated
       get :public_timeline do
         Status.limit(20)
       end
 
       desc 'Return a personal timeline.'
-      # ruleid: grape-route
+      # ruleid: grape-route-authenticated
       get :home_timeline do
         authenticate!
         current_user.statuses.limit(20)
@@ -34,7 +34,7 @@ module Twitter
         requires :id, type: Integer, desc: 'Status ID.'
       end
       route_param :id do
-        # ruleid: grape-route
+        # todoruleid: grape-route-authenticated
         get do
           Status.find(params[:id])
         end
@@ -44,7 +44,7 @@ module Twitter
       params do
         requires :status, type: String, desc: 'Your status.'
       end
-      # ruleid: grape-route
+      # ruleid: grape-route-authenticated
       post do
         authenticate!
         Status.create!({
@@ -58,7 +58,7 @@ module Twitter
         requires :id, type: String, desc: 'Status ID.'
         requires :status, type: String, desc: 'Your status.'
       end
-      # ruleid: grape-route
+      # ruleid: grape-route-authenticated
       put ':id' do
         authenticate!
         current_user.statuses.find(params[:id]).update({
@@ -71,7 +71,7 @@ module Twitter
       params do
         requires :id, type: String, desc: 'Status ID.'
       end
-      # ruleid: grape-route
+      # ruleid: grape-route-authenticated
       delete ':id' do
         authenticate!
         current_user.statuses.find(params[:id]).destroy
