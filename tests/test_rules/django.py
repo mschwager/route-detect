d776e5ee0aaa546c191e8a62d2b3a9049849becf
Matt Schwager <schwag09@gmail.com>
2023-03-05 16:57:41 -0700

    Add non-interprocedural Django authn/authz

--- tests/test_rules/django.py
+++ tests/test_rules/django.py
@@ -1,15 +1,21 @@
 from django.urls import include, path, re_path
+from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # ruleid: django-route-authenticated
-    path('index/', views.index, name='main-view'),
+    path('index/', login_required(views.index), name='main-view'),
     # ruleid: django-route-authenticated
-    path('bio/<username>/', views.bio, name='bio'),
+    path('bio/<username>/', login_required(views.bio), name='bio'),
+    # ruleid: django-route-authorized
+    path('articles/<slug:title>/', permission_required('permission')(views.article), name='article-detail'),
     # ruleid: django-route-authenticated
-    path('articles/<slug:title>/', views.article, name='article-detail'),
+    path('articles/<slug:title>/<int:section>/', login_required(views.section), name='article-section'),
     # ruleid: django-route-authenticated
-    path('articles/<slug:title>/<int:section>/', views.section, name='article-section'),
-    # ruleid: django-route-authenticated
+    path('about/', login_required(TemplateView.as_view(template_name="about.html"))),
+    # ruleid: django-route-unauthenticated
+    path('about/', TemplateView.as_view(template_name="about.html")),
+    # ruleid: django-route-unauthenticated
     path('blog/', include('blog.urls')),
     # ok: django-route-authenticated
     not_path('blog/', include('blog.urls')),
@@ -17,10 +23,10 @@ urlpatterns = [
 
 urlpatterns = [
     # ruleid: django-route-authenticated
-    re_path(r'^index/$', views.index, name='index'),
+    re_path(r'^index/$', login_required(views.index), name='index'),
     # ruleid: django-route-authenticated
-    re_path(r'^bio/(?P<username>\w+)/$', views.bio, name='bio'),
-    # ruleid: django-route-authenticated
+    re_path(r'^bio/(?P<username>\w+)/$', login_required(views.bio), name='bio'),
+    # ruleid: django-route-unauthenticated
     re_path(r'^blog/', include('blog.urls')),
     # ok: django-route-authenticated
     not_re_path('blog/', include('blog.urls')),
