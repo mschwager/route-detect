7f06019f85ed23c03344e251e9f0641f3d981405
Matt Schwager <schwag09@gmail.com>
2023-03-21 15:01:48 -0600

    Add global authenticator for CakePHP

--- tests/test_rules/cakephp.php
+++ tests/test_rules/cakephp.php
@@ -2,6 +2,12 @@
 
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Route\DashedRoute;
+use Cake\Controller\Controller;
+use Cake\Http\MiddlewareQueue;
+use Cake\Http\BaseApplication;
+
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
 
 $routes->scope('/', function (RouteBuilder $routes) {
     // ruleid: cakephp-route
@@ -17,3 +23,35 @@ $routes->scope('/', function (RouteBuilder $routes) {
     // ruleid: cakephp-route
     $routes->fallbacks(DashedRoute::class);
 });
+
+class AppController extends Controller
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        // ruleid: cakephp-route-global-authenticated
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => ['username' => 'email', 'password' => 'passwd']
+                ]
+            ]
+        ]);
+    }
+}
+
+class Application extends BaseApplication implements AuthenticationServiceProviderInterface
+{
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
+    {
+        // ruleid: cakephp-route-global-authenticated
+        $middlewareQueue->add(new ErrorHandlerMiddleware(Configure::read('Error')))
+            ->add(new AssetMiddleware())
+            ->add(new RoutingMiddleware($this))
+            ->add(new BodyParserMiddleware())
+            ->add(new AuthenticationMiddleware($this));
+
+        return $middlewareQueue;
+    }
+}
